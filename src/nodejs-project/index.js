1e4edf08e856d3c9f3028edc33e65ee135cc9765
Yury Bushev <bushevuv@gmail.com>
2018-01-10 00:46:05 +0300

    Update

--- src/nodejs-project/index.js
+++ src/nodejs-project/index.js
@@ -17,16 +17,22 @@ process.on('uncaughtException', err => {
     throw err;
 });
 
-const mongoDBUri = `mongodb://test:test@ds247077.mlab.com:47077/nodejs-mobile-testing`;
+// const mongoDBUri = `mongodb://test:test@ds247077.mlab.com:47077/nodejs-mobile-testing`;
+// const host = 'ds247077.mlab.com';
+// const port = 47077;
+
+const mongoDBUri = `mongodb://192.168.1.36:27025/admin`;
+const host       = '192.168.1.36';
+const port       = 27025;
 
 cordova.channel.on('message', msg => {
 
     if (msg === 'connect-after-tcp-ping') {
 
-        tcpp.probe('ds247077.mlab.com', 47077, (err, available) => {
+        tcpp.probe(host, port, (err, available) => {
             if (err) return log('error', err.message);
 
-            log('debug', 'available [ds247077.mlab.com]: ' + available);
+            log('debug', `Available [${host}:${port}]: ` + available);
 
             connect();
         });
@@ -38,6 +44,8 @@ cordova.channel.on('message', msg => {
 });
 
 function connect() {
+
+    log('debug', 'Connecting: ' + mongoDBUri);
 
     MongoClient.connect(mongoDBUri, (err, client) => {
         if (err) return log('error', err.message);
